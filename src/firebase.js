1e79b2fe58fb04d83bd5657658bee940d5321423
Yashodhar Patel <84219189+yashodharpatel@users.noreply.github.com>
2023-06-07 11:55:26 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCci4KaeYyX_krMGyiV_ka7ZnpZzdWdCMI",
-  authDomain: "auth-production-3ddc6.firebaseapp.com",
-  databaseURL: "https://auth-production-3ddc6-default-rtdb.firebaseio.com",
-  projectId: "auth-production-3ddc6",
-  storageBucket: "auth-production-3ddc6.appspot.com",
-  messagingSenderId: "505861880777",
-  appId: "1:505861880777:web:47bc9c17c11391af404618"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export default app;
